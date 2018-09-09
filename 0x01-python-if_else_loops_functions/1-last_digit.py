4d5dd94db64b511637e4cccb0e144ccde0cd680c
Hunt6666 <489@holbertonschool.com>
2018-09-09 19:53:56 +0000

    6 but not 0 and negatives

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,11 +7,23 @@
 import random
 number = random.randint(-10000, 10000)
 if abs(number) % 10 > 5:
-    print("Last digit of {:d} is {:d} and is greater than 5" .format(
-        number, abs(number) % 10))
-elif abs(number) % 10 < 6:
-    print("Last digit of {:d} is {:d} and is less than 6" .format(
-              number, abs(number) % 10))
+    if number >= 0:
+        print("Last digit of {:d} is {:d} and is greater than 5"
+              .format(number, abs(number) % 10))
+    else:
+        print("Last digit of {:d} is -{:d} and is greater than 5"
+              .format(number, abs(number) % 10))
+elif abs(number) % 10 < 6 and abs(number) % 10 != 0:
+    if number >= 0:
+        print("Last digit of {:d} is {:d} and is less than 6 and not 0"
+              .format(number, number % 10))
+    else:
+        print("Last digit of {:d} is -{:d} and is less than 6 and not 0"
+              .format(number, abs(number) % 10))
 else:
-    print("Last digit of {:d} is {:d} and is zero" .format(number,
+    if number >= 0:
+        print("Last digit of {:d} is {:d} and is zero" .format(number,
                                                            number % 10))
+    else:
+        print("Last digit of {:d} is -{:d} and is zero" .format(number,
+                                                           abs(number) % 10))
