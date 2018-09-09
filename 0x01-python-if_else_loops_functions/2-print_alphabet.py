c06b209f3097cc20b529da6d7ab3bff03343aa38
Hunt6666 <489@holbertonschool.com>
2018-09-09 20:10:09 +0000

    nothing is wrong except everything

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -2,4 +2,4 @@
 # prints alphabet
 # print("abcdefghijklmnopqrstuvwxyz", end = '\0')
 for i in range(0, 26):
-    print("{:s}" .format(chr(i + 97)), end='')
+    print("{}" .format(chr(i + 97)), end='')
