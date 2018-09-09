5c4604c6e8db984a98edfcc60988fdd2257e2de0
Hunt6666 <489@holbertonschool.com>
2018-09-09 20:48:57 +0000

    way easier

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    d = 1
-    if b == 0:
-        return d
-    if b < 0:
-        b = abs(b)
-        for c in range(b):
-            d = d / a
-    else:
-        for c in range(b):
-            d = d * a
-    return d
+    return(a ** b)
