cb2c557d7d2005270bacf7b761393b17a243726e
Hunt6666 <489@holbertonschool.com>
2018-09-10 19:42:56 +0000

    finally got all edge cases worked out

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -17,34 +17,40 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
 	listint_t *nhead;
 
-
-	if (head == NULL)
-		return (NULL);
-
-	nhead = malloc(sizeof(listint_t));
-	if (nhead == NULL)
-		return (NULL);
-	current = malloc(sizeof(listint_t));
-	if (current == NULL)
-		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
+	new->n = number;
+	if (*head == NULL)
+		return (new);
+	if ((*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+
 	nhead = *head;
 	current = *head;
 
-	while(current->next->n < number || current->next == NULL)
+	while(current->next != NULL && current->next->n < number)
 	{
 		current = current->next;
 		if (nhead->next == NULL)
 			break;
 		nhead = nhead->next;
 	}
+	printf("current = %d\n", current->n);
+	printf("nhead = %d\n", nhead->n);
 
-	nhead = nhead->next;
-	new->n = number;
-	new->next = nhead;
+	if (nhead != NULL)
+	{
+		nhead = nhead->next;
+		new->next = nhead;
+	}
+	else
+		new->next = NULL;
 	current->next = new;
 	return (new);
 }
