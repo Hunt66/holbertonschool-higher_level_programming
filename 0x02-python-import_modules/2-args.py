34e2bff89529e5976815e426417a9e290799e8d9
Hunt6666 <489@holbertonschool.com>
2018-09-11 19:54:14 +0000

    added safeguard

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-arguments = sys.argv[1:]
-count = len(arguments)
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i in range(count):
-    print("{}: {}".format(i + 1, arguments[i]))
+if __name__ == "__main__":
+    import sys
+    arguments = sys.argv[1:]
+    count = len(arguments)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+        for i in range(count):
+            print("{}: {}".format(i + 1, arguments[i]))
