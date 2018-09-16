c9514d50bf39175002ccd1c68d27af6566612606
Hunt6666 <489@holbertonschool.com>
2018-09-16 20:28:15 +0000

    no empty return

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 1:
         print("")
-        return
-    for i in range(0, len(matrix)):
-        for j in range(0, len(matrix[i]) - 1):
-            print("{:d}" .format(matrix[i][j]), end=' ')
-        print("{:d}".format(matrix[i][len(matrix[i]) - 1]))
+    else:
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[i]) - 1):
+                print("{:d}" .format(matrix[i][j]), end=' ')
+            print("{:d}".format(matrix[i][len(matrix[i]) - 1]))
