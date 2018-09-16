fd899cda93fbad7ee36fdfd1be83954b10de43a7
Hunt6666 <489@holbertonschool.com>
2018-09-16 19:34:22 +0000

    empty list

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return None
     tuple_a = (len(sentence), sentence[0])
+    if len(sentence) == 0:
+        tupple_a[0] = None
     return tuple_a
