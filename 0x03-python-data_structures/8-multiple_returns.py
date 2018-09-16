9aeaa987f091aaa47e49cd18e223468e3e8ceab3
Hunt6666 <489@holbertonschool.com>
2018-09-16 19:43:10 +0000

    empty string case

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = (len(sentence), sentence[0])
     if len(sentence) == 0:
-        tupple_a[0] = None
+        tuple_a = (0, None)
+    else:
+        tuple_a = (len(sentence), sentence[0])
     return tuple_a
