38803eeb16a95f3e2b80290e18586daf7189d882
Hunt6666 <489@holbertonschool.com>
2018-09-18 23:42:47 +0000

    got it now

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,35 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+
+/**
+ *reverse_listint- reverses a listint_t
+ *@head: the head of the list to be reversed
+ *Return: a pointer to new head (tail)
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+
+		(*head)->next = prev;
+
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+
+	return (*head);
+}
+
+
+
 /**
  *is_palindrome - checks to see if a listint_t is a palendrome
  *@head: head of the listint_t
@@ -9,26 +38,38 @@
 
 int is_palindrome(listint_t **head)
 {
-	int i, j, len;
+	int i;
 	listint_t *back = *head;
 	listint_t *begin = *head;
 
 	if (*head == NULL)
 		return (1);
-	for (i = 0; back->next != NULL; i++)
-		back = back->next;
-	len = i;
-	j = len;
-	for (; len > j / 2 ; len--)
+	for (i = 0; 1; i++)
 	{
-		back = *head;
-		for (i = 0; i < len; i++)
+		if (back->next == NULL)
 		{
-			back = back->next;
+			i = 1;
+			break;
 		}
-		if (back->n != begin->n)
-			return (0);
+		if (back->next->next == NULL)
+		{
+			i = 2;
+			break;
+		}
 		begin = begin->next;
+		back = back->next->next;
+	}
+	back = begin->next;
+	begin->next = NULL;
+	*head = reverse_listint(head);
+	if (i % 2 == 1)
+		*head = (*head)->next;
+	while (back != NULL && *head != NULL)
+	{
+		if (back->n != (*head)->n)
+			return (0);
+		back = back->next;
+		*head = (*head)->next;
 	}
 	return (1);
 }
