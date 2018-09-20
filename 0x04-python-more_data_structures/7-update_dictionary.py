b98985e60b8f6723577acf5d025918ae17502425
Hunt6666 <489@holbertonschool.com>
2018-09-20 19:10:34 +0000

    pep8 of 7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if a_dictionary == None:
+    if a_dictionary is None:
         a_dictionary += {'key': value}
     else:
         a_dictionary[key] = value
