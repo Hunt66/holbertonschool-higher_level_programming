3ba8acc1e983ec9d3a1f8d595a0ef70bbdcc44e1
Hunt6666 <489@holbertonschool.com>
2018-09-20 19:15:44 +0000

    this is a cheap way to do this

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    score = 0
+    for i in a_dictionary:
+        score = a_dictionary[i]
     for i in a_dictionary:
         if a_dictionary[i] > score:
             score = a_dictionary[i]
