c8b458bc283cc225001f72d404f7568832a90876
Hunt6666 <489@holbertonschool.com>
2018-09-20 19:25:19 +0000

    why

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,6 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    ans = ''
     score = a_dictionary[list(a_dictionary.keys())[0]]
     for i in a_dictionary:
         if a_dictionary[i] > score:
