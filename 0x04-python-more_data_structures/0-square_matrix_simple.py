1fcec8f0571734b288701f5b99d697d4f28a9937
Hunt6666 <489@holbertonschool.com>
2018-09-20 20:52:30 +0000

    empty check 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
+    if matrix is None or len(matrix) is 0:
+        return matrix
     m = [[None] * len(matrix[0])] * len(matrix)
     for i in range(0, len(matrix)):
         for j in range(0, len(matrix[i])):
