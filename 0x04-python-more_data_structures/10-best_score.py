63b6beb54e74b8ee114c992178cb75cc5c9abd50
Hunt6666 <489@holbertonschool.com>
2018-09-20 19:20:21 +0000

    got it to get the first key

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 def best_score(a_dictionary):
     if a_dictionary is None:
         return None
-    score = -10000
+    score = a_dictionary[next(iter(a_dictionary))]
     for i in a_dictionary:
         if a_dictionary[i] > score:
             score = a_dictionary[i]
