9cfd7aa1b8d21963a3665aee2379a638258f1b40
Hunt6666 <489@holbertonschool.com>
2018-09-20 20:26:08 +0000

    empty dict

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) is 0:
         return None
     score = -100000000000000000000000000000000000000000000000
     for i in a_dictionary:
