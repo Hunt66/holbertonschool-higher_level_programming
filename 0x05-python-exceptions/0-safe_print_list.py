781e1245bdc74d7ed45359bfdb05f398ab9ad615
Hunt6666 <489@holbertonschool.com>
2018-10-01 19:38:02 +0000

    0 fixed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(0, x):
             print(my_list[i], end="")
         print('')
-        return i
-    except:
-        for i in my_list:
-            print(i, end='')
+        return i + 1
+    except IndexError:
         print("")
         return i
