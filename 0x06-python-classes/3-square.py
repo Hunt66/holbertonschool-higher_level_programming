29b7fd1e609430a068f170d5a72104ac66f12b91
Hunt6666 <489@holbertonschool.com>
2018-10-02 23:10:00 +0000

    pep8 on 3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
     __size = 0
+
     def __init__(self, new_size=0):
         if not isinstance(new_size, int):
             self.__size = 0
@@ -10,5 +11,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = new_size
+
     def area(self):
         return (self.__size * self.__size)
