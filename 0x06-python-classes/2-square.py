7fcb1eb029d4781d705dc61e2350d8dfb775f5a1
Hunt6666 <489@holbertonschool.com>
2018-10-03 00:33:24 +0000

    changing var names

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 class Square:
 
-    def __init__(self, new_size=0):
-        if type(new_size) is not int:
+    def __init__(self, size=0):
+        if type(size) is not int:
             self.__size = 0
             raise TypeError("size myst be an integer")
-        elif new_size < 0:
+        elif size < 0:
             self.size = 0
             raise ValueError("size must be >= 0")
         else:
-            self.__size = new_size
+            self.__size = size
