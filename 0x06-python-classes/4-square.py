c9548b4d257284b6dffbf9994a4ce20a7860fac0
Hunt6666 <489@holbertonschool.com>
2018-10-03 00:14:00 +0000

    multi redundency

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -31,4 +31,6 @@ class Square:
     def area(self):
         if not isinstance(self.size, int):
             raise TypeError("size must be an integer")
+        if self.size < 0:
+            raise ValueError("size must be >= 0")
         return (self.size * self.size)
