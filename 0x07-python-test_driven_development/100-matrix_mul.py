9219f1338ea7ab4b22bad90e84196621337379fa
Hunt6666 <489@holbertonschool.com>
2018-10-04 21:57:06 +0000

    all error checks added on 100 maby not in order

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -18,33 +18,25 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_b can't be empty")
     out = []
     crnt = 0
+    for i in range(0, len(m_b)):
+        if len(m_b[0]) != len(m_b[i]):
+            raise TypeError("each row of m_b must should be of the same size")
+
     for i in range(0, len(m_a)):
+        if len(m_a[0]) != len(m_a[i]):
+            raise TypeError("each row of m_a must should be of the same size")
         out1 = []
         for j in range(0, len(m_b[i])):
+            if not isinstance(m_b[i][j], int) and
+            not isinstance(m_b[i][j], float):
+                raise ValueError("m_b should contain only integers or floats")
             crnt = 0
             for k in range(0, len(m_a[i])):
+                if not isinstance(m_a[i][k], int) and
+                not isinstance(m_a[i][k], float):
+                    raise ValueError("m_a should contain only integers "
+                                     "or floats")
                 crnt += m_a[i][k] * m_b[k][j]
             out1.append(crnt)
         out.append(out1)
     return out
-
-
-
-
-
-
-
-#    for i in range(0, len(m_a)):
- #       out1 = []
-  #      if len(m_a[0]) != len(m_a[i]):
-   #         raise TypeError("each row of m_a must should be of the same size")
-    #    for j in range(0, len(m_a[i])):
-     #       if not isinstance(m_a[i][j], int) and
-      #      not isinstance(m_a[i][j], float):
-       #         raise TypeError("m_a should contain only integers or floats")
-        #    for k in range(0, len(m_b)):
-         #       if not isinstance(m_b[k][j], int) and
-          #      not isinstance(m_b[k][j], float):
-           #         raise TypeError("m_b should contain only integers or "
-            #                        "floats")
-                
