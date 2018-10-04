5cd56af7543be5e17678f4059c64f7ec05be59a9
Hunt6666 <489@holbertonschool.com>
2018-10-04 21:47:24 +0000

    figured out algorithem for multiply matrix

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -16,6 +16,35 @@ def matrix_mul(m_a, m_b):
         raise ValueError("m_a can't be empty")
     if m_b == [] or m_a == [[]]:
         raise ValueError("m_b can't be empty")
-    length_a = len(m_a[0])
     out = []
-    
+    crnt = 0
+    for i in range(0, len(m_a)):
+        out1 = []
+        for j in range(0, len(m_b[i])):
+            crnt = 0
+            for k in range(0, len(m_a[i])):
+                crnt += m_a[i][k] * m_b[k][j]
+            out1.append(crnt)
+        out.append(out1)
+    return out
+
+
+
+
+
+
+
+#    for i in range(0, len(m_a)):
+ #       out1 = []
+  #      if len(m_a[0]) != len(m_a[i]):
+   #         raise TypeError("each row of m_a must should be of the same size")
+    #    for j in range(0, len(m_a[i])):
+     #       if not isinstance(m_a[i][j], int) and
+      #      not isinstance(m_a[i][j], float):
+       #         raise TypeError("m_a should contain only integers or floats")
+        #    for k in range(0, len(m_b)):
+         #       if not isinstance(m_b[k][j], int) and
+          #      not isinstance(m_b[k][j], float):
+           #         raise TypeError("m_b should contain only integers or "
+            #                        "floats")
+                
