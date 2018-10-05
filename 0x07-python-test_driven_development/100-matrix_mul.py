fc43514e3fc95928aa0d38a6762be149276aac8e
Hunt6666 <489@holbertonschool.com>
2018-10-05 11:07:21 -0700

    save

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -8,25 +8,42 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if not isinstance(m_a[0], list):
+    try:
+        if not isinstance(m_a[0], list):
+            raise TypeError("m_a must be a list of lists")
+    except:
         raise TypeError("m_a must be a list of lists")
-    if not isinstance(m_b[0], list):
+    try:
+        if not isinstance(m_b[0], list):
+            raise TypeError("m_b must be a list of lists")
+    except:
         raise TypeError("m_b must be a list of lists")
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if m_b == [] or m_a == [[]]:
+    if m_b == [] or m_b == [[]]:
         raise ValueError("m_b can't be empty")
     out = []
     crnt = 0
     for i in range(0, len(m_b)):
+        for j in range(0, len(m_b[i])):
+            if (not isinstance(m_b[i][j], int)
+            and not isinstance(m_b[i][j], float)):
+                raise TypeError("m_b should contain only integers or floats")
+    for i in range(0, len(m_a)):
+        for j in range(0, len(m_a[i])):
+            if (not isinstance(m_a[i][j], int)
+            and not isinstance(m_a[i][j], float)):
+                raise TypeError("m_a should contain only integers or floats")
+    for i in range(0, len(m_b)):
         if len(m_b[0]) != len(m_b[i]):
             raise TypeError("each row of m_b must should be of the same size")
-    if (len(m_a) < len(m_b)):
-        raise ValueError("m_a and m_b can't be multiplied")
-
     for i in range(0, len(m_a)):
         if len(m_a[0]) != len(m_a[i]):
             raise TypeError("each row of m_a must should be of the same size")
+    if (len(m_a) > len(m_b)):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    for i in range(0, len(m_a)):
         out1 = []
         for j in range(0, len(m_b[i])):
             crnt = 0
