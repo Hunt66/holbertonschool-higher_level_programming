7155b8064f719faf5690cd76dde5f948438223ff
Hunt6666 <489@holbertonschool.com>
2018-10-10 10:23:16 -0700

    more tests

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[1,2,3],[1,None, 6]]
+matrix = [[1,2,3],[1,3, 6]]
 
 try:
-    print(matrix_divided(matrix, "str"))
+    print(matrix_divided(matrix, float('inf')))
 except Exception as e:
     print(e)
 print(matrix)
