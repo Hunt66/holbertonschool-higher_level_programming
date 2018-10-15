dd5ccb8a137e6564b10fabe573ef7893be751984
Hunt6666 <489@holbertonschool.com>
2018-10-15 10:55:59 -0700

    all mandatroy done

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+class BaseGeometry():
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return (self.__height * self.__width)
+
+    def __str__(self):
+        st = "[Rectangle] "
+        st = st + str(self.__width) + '/' + str(self.__height)
+        return st
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+        self._Rectangle__width = size
+        self._Rectangle__height = size
+
+    def __str__(self):
+        st = "[Square] "
+        st = st + str(self._Rectangle__width) + '/'
+        st += str(self._Rectangle__height)
+        return st
