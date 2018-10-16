0404fe336b922b478229507301ce2e28ec803f9a
Hunt6666 <489@holbertonschool.com>
2018-10-16 15:02:42 -0700

    fixed 9 and added more

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,6 +3,9 @@ save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 import sys
 args = sys.argv[1:]
-with open("add_items.json", 'w') as f:
-    stuff = load_from_json_file(sys.argv[1:])
-    save_to_json_file(stuff, f)
+try:
+    stuff = load_from_json_file("add_item.json")
+    stuff = stuff + args
+    save_to_json_file(stuff, "add_item.json")
+except:
+    save_to_json_file(args, "add_item.json")
