9e2cc4c69c7f591d6a4f3cd0395eb9ceffec0f48
Hunt6666 <489@holbertonschool.com>
2018-10-16 17:59:38 -0700

    100 innitial commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, 'r+') as f:
+        st = ''
+        for line in (f.readlines()):
+            st += line
+            if search_string in line:
+                st += new_string
+        f.write(st)
