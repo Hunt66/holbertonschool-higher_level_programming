b1f5933b179c70affce2939aa9e549acf964dcb4
Hunt6666 <489@holbertonschool.com>
2018-10-17 08:49:09 -0700

    works

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -6,4 +6,5 @@ def append_after(filename="", search_string="", new_string=""):
             st += line
             if search_string in line:
                 st += new_string
+        f.seek(0)
         f.write(st)
