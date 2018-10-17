5240b071b5b2829c75f52ed5b6bd1e79e5097c65
Hunt6666 <489@holbertonschool.com>
2018-10-16 17:34:10 -0700

    pascals triangle

---
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    current = [1]
+    new = []
+    ret = [[]]
+    for i in range(0, n):
+        ret += [current]
+        for j in range(0, len(current) + 1):
+            if j == 0:
+                new += [1]
+            elif j == len(current):
+                new += [1]
+            else:
+                new += [(current[j] + current[j - 1])]
+        current = new.copy()
+        new = []
+    return(ret)
