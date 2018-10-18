fbdc7034a89c1c3f438c6b7ad58ce7b328b22d4f
Hunt6666 <489@holbertonschool.com>
2018-10-17 17:08:17 -0700

    json

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -2,7 +2,6 @@
 def to_json_string(my_obj):
     import json
     try:
-        json.dumps(my_obj)
-        return str(my_obj)
+        return json.dumps(my_obj)
     except:
         raise TypeError("{} is not JSON serializable".format(my_obj))
