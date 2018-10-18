3659ff9ef2153b778f6cad3487d130522e877343
Hunt6666 <489@holbertonschool.com>
2018-10-18 14:11:11 -0700

    up to 12 work

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
-class Base():
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-
+from models.base import Base
 
 class Rectangle(Base):
 
@@ -65,11 +56,50 @@ class Rectangle(Base):
         return (self.__width * self.__height)
 
     def display(self):
+        for i in range(0, self.__y):
+            print('')
         for i in range(0, self.__height):
+            for j in range(0, self.__x):
+                print(' ', end='')
             for j in range(0, self.__width):
                 print('#', end='')
             if i != self.__height:
                 print('')
+
+    def __str__(self):
+        st = "[Rectangle] (" + str(self.id) + ') ' + str(self.__x) + '/'
+        st += str(self.__y)
+        st += " - " + str(self.__width) + '/' + str(self.__height)
+        return st
+
+    def update(self, *args, **kwargs):
+        if (args) and args != ():
+            if len(args) >= 1:
+                super(Rectangle, self).__init__(args[0])
+            if len(args) >= 2:
+                self.width = args[1]
+            if len(args) >= 3:
+                self.height = args[2]
+            if len(args) >= 4:
+                self.x = args[3]
+            if len(args) >= 5:
+                self.y = args[4]
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    super(Rectangle, self).__init__(v)
+                if k == "width":
+                    self.width = v
+                if k == "height":
+                    self.height = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
+
+
+    def to_dictionary(self):
+        return self.__dict__
 
     def __init__(self, width, height, x=0, y=0, id=None):
         super(Rectangle, self).__init__(id)
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+from models.base import Base
+
+class Square(Rectangle):
+
+    def __str__(self):
+        st = "[Square] (" + str(self.id) + ') '
+        st += str(self._Rectangle__x) + '/' + str(self._Rectangle__y)
+        st += ' - ' + str(self._Rectangle__width)
+        return st
+
+    @property
+    def size(self):
+        return self._Rectangle__width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        if (args) and args != ():
+            if len(args) >= 1:
+                super(Rectangle, self).__init__(args[0])
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for k, v in kwargs.items():
+                if k == "id":
+                    super(Rectangle, self).__init__(v)
+                if k == "size":
+                    self.size = v
+                if k == "x":
+                    self.x = v
+                if k == "y":
+                    self.y = v
+
+    def to_dictionary(self):
+        return self.__dict__
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super(Square, self).__init__(size, size, x, y, id)
