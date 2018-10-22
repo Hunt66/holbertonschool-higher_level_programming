bd4153a87991057cffae2083f5fec2a0a811048c
Hunt6666 <489@holbertonschool.com>
2018-10-22 14:33:06 -0700

    pep8 and comments

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Unittest for rectangle class
-"""
+"""Unittest for rectangle class"""
+
 
 import unittest
 import sys
@@ -12,8 +12,9 @@ from models.base import Base
 
 @contextmanager
 def cap_out():
+    """ captures print output"""
     nout = io.StringIO()
-    nerr =  io.StringIO()
+    nerr = io.StringIO()
     oout = sys.stdout
     oerr = sys.stderr
     try:
@@ -24,9 +25,12 @@ def cap_out():
         sys.stdout = oout
         sys.stderr = oerr
 
+
 class TestRectangle(unittest.TestCase):
+    """ test class for rectangle"""
 
     def setUp(self):
+        """setup for tests"""
         self.rec0 = Rectangle(5, 4)
         self.rec1 = Rectangle(2, 1)
         self.rec2 = Rectangle(3, 6, 0, 0, 13)
@@ -37,6 +41,7 @@ class TestRectangle(unittest.TestCase):
         self.rec7 = Rectangle(3, 3, 3, 3, 256)
 
     def test_display(self):
+        """ tests display"""
         with cap_out() as (out, err):
             self.rec1.display()
             buf = out.getvalue()
@@ -51,6 +56,7 @@ class TestRectangle(unittest.TestCase):
             self.assertEqual(buf, ('\n' * 6) + ('     #\n' * 2))
 
     def test_to_dictionary(self):
+        """ tests the to dictionary"""
         self.assertEqual(self.rec5.to_dictionary(),
                          {'x': 1, 'y': 1, 'width': 1, 'height': 1, 'id': 64})
         self.assertEqual(self.rec6.to_dictionary(),
@@ -59,18 +65,21 @@ class TestRectangle(unittest.TestCase):
                          {'x': 3, 'y': 3, 'width': 3, 'height': 3, 'id': 256})
 
     def test_id(self):
+        """ tests id"""
         self.assertEqual(self.rec0.id, 7)
         self.assertEqual(self.rec1.id, 8)
         self.assertEqual(self.rec2.id, 13)
         self.assertEqual(self.rec3.id, 5)
 
     def test_str(self):
+        """ tests the __str__ """
         self.assertEqual(str(self.rec0), "[Rectangle] (9) 0/0 - 5/4")
         self.assertEqual(str(self.rec1), "[Rectangle] (10) 0/0 - 2/1")
         self.assertEqual(str(self.rec2), "[Rectangle] (13) 0/0 - 3/6")
         self.assertEqual(str(self.rec3), "[Rectangle] (5) 5/6 - 1/2")
 
     def test_update(self):
+        """ tests update function """
         self.rec4.update(89)
         self.assertEqual(str(self.rec4), "[Rectangle] (89) 1/1 - 1/1")
         self.rec4.update(89, 2)
@@ -90,38 +99,46 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(str(self.rec4), "[Rectangle] (89) 3/3 - 3/3")
 
     def test_area(self):
+        """ tests area"""
         self.assertEqual(self.rec0.area(), 20)
         self.assertEqual(self.rec1.area(), 2)
         self.assertEqual(self.rec2.area(), 18)
         self.assertEqual(self.rec3.area(), 2)
 
     def test_width(self):
+        """ tests width"""
         self.assertEqual(self.rec0.width, 5)
         self.assertEqual(self.rec1.width, 2)
         self.assertEqual(self.rec2.width, 3)
         self.assertEqual(self.rec3.width, 1)
 
     def test_height(self):
+        """ tests height"""
         self.assertEqual(self.rec0.height, 4)
         self.assertEqual(self.rec1.height, 1)
         self.assertEqual(self.rec2.height, 6)
         self.assertEqual(self.rec3.height, 2)
 
     def test_x(self):
+        """ tests x"""
         self.assertEqual(self.rec0.x, 0)
         self.assertEqual(self.rec1.x, 0)
         self.assertEqual(self.rec2.x, 0)
         self.assertEqual(self.rec3.x, 5)
 
     def test_y(self):
+        """ tests y"""
         self.assertEqual(self.rec3.y, 6)
         self.assertEqual(self.rec0.y, 0)
         self.assertEqual(self.rec1.y, 0)
         self.assertEqual(self.rec2.y, 0)
 
+
 class TestErrorRec(unittest.TestCase):
+    """ test class for rectangle errors"""
 
     def test_width_Type_error(self):
+        """ width type errors"""
         self.assertRaises(TypeError, Rectangle, "1", 1, 1, 1, 1)
         self.assertRaises(TypeError, Rectangle, (1, 1), 1, 1, 1, 1)
         self.assertRaises(TypeError, Rectangle, {'1': 1}, 1, 1, 1, 1)
@@ -133,6 +150,7 @@ class TestErrorRec(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, None, 1, 1, 1, 1)
 
     def test_height_Type_error(self):
+        """ height type errors"""
         self.assertRaises(TypeError, Rectangle, 1, "1", 1, 1, 1)
         self.assertRaises(TypeError, Rectangle, 1, (1, 1), 1, 1, 1)
         self.assertRaises(TypeError, Rectangle, 1, {'1': 1}, 1, 1, 1)
@@ -144,6 +162,7 @@ class TestErrorRec(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 1, None, 1, 1, 1)
 
     def test_x_Type_error(self):
+        """ x type erors"""
         self.assertRaises(TypeError, Rectangle, 1, 1, "1", 1, 1)
         self.assertRaises(TypeError, Rectangle, 1, 1, (1, 1), 1, 1)
         self.assertRaises(TypeError, Rectangle, 1, 1, {'1': 1}, 1, 1)
@@ -155,6 +174,7 @@ class TestErrorRec(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 1, 1,  None, 1, 1)
 
     def test_y_Type_error(self):
+        """ y type errors"""
         self.assertRaises(TypeError, Rectangle, 1, 1, 1, "1", 1)
         self.assertRaises(TypeError, Rectangle, 1, 1, 1, (1, 1), 1)
         self.assertRaises(TypeError, Rectangle, 1, 1, 1, {'1': 1}, 1)
@@ -166,15 +186,19 @@ class TestErrorRec(unittest.TestCase):
         self.assertRaises(TypeError, Rectangle, 1, 1, 1,  None, 1)
 
     def test_width_Value_error(self):
+        """ width val errors"""
         self.assertRaises(ValueError, Rectangle, -1, 1, 1, 1, 1)
         self.assertRaises(ValueError, Rectangle, 0, 1, 1, 1, 1)
 
     def test_height_Value_error(self):
+        """ height val errors"""
         self.assertRaises(ValueError, Rectangle, 1, -1, 1, 1, 1)
         self.assertRaises(ValueError, Rectangle, 1, 0, 1, 1, 1)
 
     def test_x_Value_error(self):
+        """ x val errors"""
         self.assertRaises(ValueError, Rectangle, 1, 1, -1, 1, 1)
 
     def test_y_Value_error(self):
+        """ y value errors"""
         self.assertRaises(ValueError, Rectangle, 1, 1, 1, -1, 1)
--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""Unittest for square class
-"""
+"""Unittest for square class"""
+
 
 import unittest
 import sys
@@ -9,10 +9,12 @@ from contextlib import contextmanager
 from models.square import Square
 from models.base import Base
 
+
 @contextmanager
 def cap_out():
+    """ captures print output"""
     nout = io.StringIO()
-    nerr =  io.StringIO()
+    nerr = io.StringIO()
     oout = sys.stdout
     oerr = sys.stderr
     try:
@@ -23,9 +25,12 @@ def cap_out():
         sys.stdout = oout
         sys.stderr = oerr
 
+
 class TestSquare(unittest.TestCase):
+    """ test class for square"""
 
     def setUp(self):
+        """ setup for tests"""
         self.rec0 = Square(5)
         self.rec1 = Square(2)
         self.rec2 = Square(6, 0, 0, 13)
@@ -36,6 +41,7 @@ class TestSquare(unittest.TestCase):
         self.rec7 = Square(3, 3, 3, 256)
 
     def test_display(self):
+        """ tests display"""
         with cap_out() as (out, err):
             self.rec0.display()
             buf = out.getvalue()
@@ -50,6 +56,7 @@ class TestSquare(unittest.TestCase):
             self.assertEqual(buf, ('\n' * 6) + ('     ##\n' * 2))
 
     def test_to_dictionary(self):
+        """ tests the to dictionary"""
         self.assertEqual(self.rec5.to_dictionary(),
                          {'x': 1, 'y': 1, 'size': 1, 'id': 64})
         self.assertEqual(self.rec6.to_dictionary(),
@@ -58,18 +65,21 @@ class TestSquare(unittest.TestCase):
                          {'x': 3, 'y': 3, 'size': 3, 'id': 256})
 
     def test_id(self):
+        """ tests id"""
         self.assertEqual(self.rec0.id, 5)
         self.assertEqual(self.rec1.id, 6)
         self.assertEqual(self.rec2.id, 13)
         self.assertEqual(self.rec3.id, 5)
 
     def test_str(self):
+        """ tests __str__"""
         self.assertEqual(str(self.rec0), "[Square] (9) 0/0 - 5")
         self.assertEqual(str(self.rec1), "[Square] (10) 0/0 - 2")
         self.assertEqual(str(self.rec2), "[Square] (13) 0/0 - 6")
         self.assertEqual(str(self.rec3), "[Square] (5) 5/6 - 2")
 
     def test_update(self):
+        """ tests update fucntion"""
         self.rec4.update(89)
         self.assertEqual(str(self.rec4), "[Square] (89) 1/1 - 1")
         self.rec4.update(89, 2)
@@ -87,32 +97,39 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(str(self.rec4), "[Square] (89) 3/3 - 3")
 
     def test_area(self):
+        """ tests area"""
         self.assertEqual(self.rec0.area(), 25)
         self.assertEqual(self.rec1.area(), 4)
         self.assertEqual(self.rec2.area(), 36)
         self.assertEqual(self.rec3.area(), 4)
 
     def test_size(self):
+        """ tests size"""
         self.assertEqual(self.rec0.size, 5)
         self.assertEqual(self.rec1.size, 2)
         self.assertEqual(self.rec2.size, 6)
         self.assertEqual(self.rec3.size, 2)
 
     def test_x(self):
+        """ tests x"""
         self.assertEqual(self.rec0.x, 0)
         self.assertEqual(self.rec1.x, 0)
         self.assertEqual(self.rec2.x, 0)
         self.assertEqual(self.rec3.x, 5)
 
     def test_y(self):
+        """ tests y"""
         self.assertEqual(self.rec3.y, 6)
         self.assertEqual(self.rec0.y, 0)
         self.assertEqual(self.rec1.y, 0)
         self.assertEqual(self.rec2.y, 0)
 
+
 class TestErrorSq(unittest.TestCase):
+    """error test class"""
 
     def test_size_Type_error(self):
+        """ tests size type error"""
         self.assertRaises(TypeError, Square, "1", 1, 1, 1)
         self.assertRaises(TypeError, Square, (1, 1), 1, 1, 1)
         self.assertRaises(TypeError, Square, {'1': 1}, 1, 1, 1)
@@ -124,6 +141,7 @@ class TestErrorSq(unittest.TestCase):
         self.assertRaises(TypeError, Square, None, 1, 1, 1)
 
     def test_x_Type_error(self):
+        """ tests x type error"""
         self.assertRaises(TypeError, Square, 1, "1", 1, 1)
         self.assertRaises(TypeError, Square, 1, (1, 1), 1, 1)
         self.assertRaises(TypeError, Square, 1, {'1': 1}, 1, 1)
@@ -135,6 +153,7 @@ class TestErrorSq(unittest.TestCase):
         self.assertRaises(TypeError, Square, 1, None, 1, 1)
 
     def test_y_Type_error(self):
+        """ tests y typeerror"""
         self.assertRaises(TypeError, Square, 1, 1, "1", 1)
         self.assertRaises(TypeError, Square, 1, 1, (1, 1), 1)
         self.assertRaises(TypeError, Square, 1, 1, {'1': 1}, 1)
@@ -146,11 +165,14 @@ class TestErrorSq(unittest.TestCase):
         self.assertRaises(TypeError, Square, 1, 1,  None, 1)
 
     def test_size_Value_error(self):
+        """ tests size val error"""
         self.assertRaises(ValueError, Square, -1, 1, 1, 1)
         self.assertRaises(ValueError, Square, 0, 1, 1, 1)
 
     def test_x_Value_error(self):
+        """ tests x val error"""
         self.assertRaises(ValueError, Square, 1, -1, 1, 1)
 
     def test_y_Value_error(self):
+        """ tests y val error"""
         self.assertRaises(ValueError, Square, 1, 1, -1, 1)
