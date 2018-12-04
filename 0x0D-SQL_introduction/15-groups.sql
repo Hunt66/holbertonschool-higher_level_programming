71c42fd9579d39bcc009a054eadf815192ccd316
Hunt6666 <489@holbertonschool.com>
2018-12-03 22:23:36 -0800

    misspell

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- shows the number of people who have certain scores in second_table
-SELECT score, cout(score) AS number FROM second_table GROUP BY score ORDER BY score DESC
+SELECT score, count(*) AS number FROM second_table GROUP BY score ORDER BY score DESC
