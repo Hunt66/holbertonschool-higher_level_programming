f324ba523be2a36a665754a8463e7ca9b5fc6130
Hunt6666 <489@holbertonschool.com>
2019-01-03 12:58:35 -0800

    11 innitial

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" Counts instences of a certain state """
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+import sys
+
+
+def main(argv):
+    """func - main - args"""
+    if len(argv) != 4:
+        print("Enter 3 arguments")
+        return
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                       argv[2],
+                                                                       argv[3]
+                                                                       ),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    new = State(name='Lousiana')
+    session.add(new)
+    session.commit()
+    print(new.id)
+
+    session.close()
+
+if __name__ == "__main__":
+    import sys
+    main(sys.argv)
