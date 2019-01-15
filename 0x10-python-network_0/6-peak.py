3c385716c7b24437bc2c9fb90be26d08b7dcb49b
Hunt6666 <489@holbertonschool.com>
2019-01-15 15:07:10 -0800

    more efficient

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -7,8 +7,4 @@ def find_peak(list_of_integers):
     if len(list_of_integers) == 0:
         return None
     else:
-        max = list_of_integers[0]
-        for i in list_of_integers:
-            if max < i:
-                max = i
-        return max
+        return max(list_of_integers)
