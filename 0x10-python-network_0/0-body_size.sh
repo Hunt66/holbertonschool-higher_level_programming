0b1d011768b5a0b7162165e0a152a97614d38009
Hunt6666 <489@holbertonschool.com>
2019-01-15 13:33:05 -0800

    rm

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # accepts URL sends request to display size of body of response
-#if [ $1 ]; then
 curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
-#else
-#    echo "requires input"
-#fi
