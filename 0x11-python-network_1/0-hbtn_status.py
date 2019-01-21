2c47b32bc828694d0d2b54ca75316a0c7ddaa1ba
Hunt6666 <489@holbertonschool.com>
2019-01-21 13:13:58 -0800

    tabs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,6 +5,6 @@ import urllib.request
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
     html = r.read()
     print("Body response:")
-    print("    - type: " + str(type(html)))
-    print("    - content: " + str(html))
-    print("    - utf8 content: " + str(html.decode('utf-8')))
+    print("\t- type: " + str(type(html)))
+    print("\t- content: " + str(html))
+    print("\t- utf8 content: " + str(html.decode('utf-8')))
