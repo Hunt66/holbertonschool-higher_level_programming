71cd07ad7c63598176f82aaea0629a3a823ff55d
Hunt6666 <489@holbertonschool.com>
2019-01-21 13:41:34 -0800

    utf8

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: " + str(type(r.text)))
-    print("\t- content: " + str(r.content))
+    print("\t- content: " + str(r.content.decode('utf-8')))
