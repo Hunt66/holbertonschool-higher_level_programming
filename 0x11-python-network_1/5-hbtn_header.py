571a4dbb3d37c8cab29cea9ff0964cda2ad528d0
Hunt6666 <489@holbertonschool.com>
2019-01-21 13:38:55 -0800

    name main

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,8 +4,9 @@ import requests
 import sys
 
 
-if len(sys.argv) < 2:
-    print("requires URL")
-else:
-    r = requests.get(sys.argv[1])
-    print(r.headers['X-Request-Id'])
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("requires URL")
+    else:
+        r = requests.get(sys.argv[1])
+        print(r.headers['X-Request-Id'])
