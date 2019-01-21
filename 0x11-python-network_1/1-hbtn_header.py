b9adc5017998fdcb5847ca27670afebc9df4b167
Hunt6666 <489@holbertonschool.com>
2019-01-21 13:24:49 -0800

    error back

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,8 @@
 import urllib.request
 import sys
 
-with urllib.request.urlopen(sys.argv[1]) as r:
-    print(r.info()['X-Reqest-Id'])
+if len(sys.argv) < 2:
+    print("URL required")
+else:
+    with urllib.request.urlopen(sys.argv[1]) as r:
+        print(r.info()['X-Reqest-Id'])
