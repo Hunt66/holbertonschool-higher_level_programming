ef4dcf5d32893791210b56766c647b2bab407826
Hunt6666 <489@holbertonschool.com>
2019-01-21 15:54:20 -0800

    no len check

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,16 +5,13 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("No result")
-    else:
-        lett = str(sys.argv[1])
-        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': lett})
-        try:
-            ans = r.json()
-            if ans is not None:
-                print('[' + str(ans['id']) + '] ' + str(ans['name']))
-            else:
-                print("No result")
-        except:
+    lett = str(sys.argv[1])
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': lett})
+    try:
+        ans = r.json()
+        if ans is not None:
+            print('[' + str(ans['id']) + '] ' + str(ans['name']))
+        else:
             print("No result")
+    except:
+        print("No result")
