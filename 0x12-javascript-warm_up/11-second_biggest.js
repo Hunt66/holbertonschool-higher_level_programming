6188c0bae5c1cd088cf63e9bab8623bbaf15dc45
Hunt6666 <489@holbertonschool.com>
2019-02-12 15:08:31 -0800

    11 all good

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// searches the second biggest int passed as an arg
+let argv = process.argv;
+let i = 0;
+let newa = [];
+if ((argv.length < 4 && parseInt(argv[2]) >= 0) || !argv[2]) {
+  console.log('0');
+}
+for (i = 2; i < argv.length; i++) {
+  newa.push(parseInt(argv[i]));
+} newa = [...new Set(newa)].sort((a, b) => a - b);
+console.log(newa[newa.length - 2]);
