cbf6959eccf04ba76bd9889533478326142f3046
Hunt6666 <489@holbertonschool.com>
2019-02-12 13:01:03 -0800

    2 done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// prints weather arguments were found
+let argc = process.argv.length;
+if (argc === 2)
+{
+    console.log('No argument');
+}
+else if (argc === 3)
+{
+    console.log('Argument found');
+}
+else
+{
+    console.log('Arguments found');
+}
