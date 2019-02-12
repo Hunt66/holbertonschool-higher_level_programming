f9360a829e431d6bc5452e0d2a6ae187fa6ca74b
Hunt6666 <489@holbertonschool.com>
2019-02-12 14:06:28 -0800

    9 all goood

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// prints the addition of two ints given as args
+let num1 = parseInt(process.argv[2]);
+let num2 = parseInt(process.argv[3]);
+let num3 = num1 + num2;
+console.log(num3);
