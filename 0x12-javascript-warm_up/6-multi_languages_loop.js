02fefe8598b5d8f78fdb6f61af2c0cf5f534f55f
Hunt6666 <489@holbertonschool.com>
2019-02-12 13:49:16 -0800

    why do we have to define it at top if it works without that

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// uses array to pring same as prob 1
+let i = 0;
+let lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
