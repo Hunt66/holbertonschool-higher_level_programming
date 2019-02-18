73b77b434e8c210abff8f4093f9dcc0f8f0524d2
Hunt6666 <489@holbertonschool.com>
2019-02-18 15:24:46 -0800

    messed up

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -3,11 +3,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (h <= 0 || h <= 0) {
+    if (h <= 0 || w <= 0 || h === undefined || w === undefined) {
       return this;
     }
-    this.height = h;
     this.width = w;
+    this.height = h;
   }
   print () {
     let p = '';
